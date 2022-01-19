08a7450ccdccc775c78e753f4bf12a0fea2e5560
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2022-01-19 17:54:12 -0500

    inicializando proyecto

---
+++ src/main/java/co/com/sofka/dataBank/DataBankApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.dataBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/dataBank/DataBankApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.dataBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
